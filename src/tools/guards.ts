bed01512279bfa752ec1e14789eff7d0bacf62bd
wmakeev <w.makeev@gmail.com>
2026-02-16 16:26:39 +0500

    docs(todo)

--- src/tools/guards.ts
+++ src/tools/guards.ts
@@ -7,9 +7,14 @@ export function isObject(val: unknown): val is object {
   return objToStrProto.call(val) === '[object Object]'
 }
 
+// TODO Для разных случаев может потребоваться отдельный вариант `isSimpleValue` проверки.
+// Вероятно стоит сделать несколько исходя из конкретной практической потребности.
+
 export function isSimpleValue(
   val: unknown
 ): val is number | string | boolean | bigint | null | undefined {
+  // TODO Разделять на функции удобно, но приходится делать лишние вызовы и
+  // дополнительные проверки в performance critical функции.
   val = unbox(val)
 
   const type = typeof val
